fc69689fd19b0a0019126fc36de37de4c50ad0ea
kjh3165 <jhwk109@gmail.com>
2026-06-10 12:54:52 +0900

    백: Ut.cmd.run 메서드 만들어서 도입

--- backend/src/main/java/com/restapi/global/initData/DevInitData.java
+++ backend/src/main/java/com/restapi/global/initData/DevInitData.java
@@ -1,5 +1,6 @@
 package com.restapi.global.initData;
 
+import com.restapi.standard.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -16,37 +17,14 @@ public class DevInitData {
     @Bean
     ApplicationRunner devInitDataApplicationRunner() {
         return args -> {
-            boolean isWindows = System
-                    .getProperty("os.name")
-                    .toLowerCase()
-                    .contains("win");
-
-            ProcessBuilder builder = new ProcessBuilder(
-                    "npx" + (isWindows ? ".cmd" : ""),
+            Ut.cmd.runAsync(
+                    "npx{{DOT_CMD}}",
                     "--yes",
                     "--package", "typescript",
                     "--package", "openapi-typescript",
                     "openapi-typescript", "http://localhost:8080/v3/api-docs/apiV1",
                     "-o", "../frontend/src/lib/backend/apiV1/schema.d.ts"
             );
-
-            // 에러 스트림도 출력 스트림과 함께 병합
-            builder.redirectErrorStream(true);
-
-            // 프로세스 시작
-            Process process = builder.start();
-
-            // 결과 출력
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line); // 결과 한 줄씩 출력
-                }
-            }
-
-            // 종료 코드 확인
-            int exitCode = process.waitFor();
-            System.out.println("종료 코드: " + exitCode);
         };
     }
 }
